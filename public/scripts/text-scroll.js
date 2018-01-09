348e29e109a0b7940421483cb74878d27f7e39ec
RobertG-H <ultraconga@hotmail.com>
2018-01-09 18:19:18 -0500

    Created basic template, missing content and javascript

---
+++ public/scripts/text-scroll.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  if($('.navbar').length > 0){
+      $(window).on("scroll load resize", function(){
+          checkScroll();
+      });
+  }
+});
+
+/**
+ * Listen to scroll to change header opacity class
+ */
+
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+    if($(window).scrollTop() < startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
